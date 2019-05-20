f177a3e1823ccdf3dacbbe3ef974f1087e685933
Felipe de Mello <13694949+fdemello@users.noreply.github.com>
2019-05-20 01:34:50 -0400

    Update express.js
    Adding Access-Control-Allow-Origin header to prevent Access to XMLHttpRequest at 'https://host:port/p/pad/export/markdown' from origin 'https://otherhost' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- express.js
+++ express.js
@@ -6,6 +6,7 @@ exports.expressCreateServer = function (hook_name, args, cb) {
     var revision = req.params.rev ? req.params.rev : null;
 
     exportMarkdown.getPadMarkdownDocument(padID, revision, function(err, result) {
+      res.header('Access-Control-Allow-Origin', '*');
       res.contentType('plain/text');
       res.send(result);
     });
