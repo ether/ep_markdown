d0cf0682d710543e6601b46e5056feb054087827
Richard Hansen <rhansen@rhansen.org>
2021-06-18 03:25:15 -0400

    Delete unnecessary `markdown` wrapper object

--- static/js/markdown.js
+++ static/js/markdown.js
@@ -1,35 +1,33 @@
 'use strict';
 
 exports.postAceInit = (hookName, context) => {
-  const markdown = {
-    enable: () => {
-      // add css class markdown
-      $('iframe[name="ace_outer"]').contents().find('iframe')
-          .contents().find('#innerdocbody').addClass('markdown');
-      $('#underline').hide(); // no markdown support for these
-      $('#strikethrough').hide();
-    },
-    disable: () => {
-      // add css class markdown
-      $('iframe[name="ace_outer"]').contents().find('iframe')
-          .contents().find('#innerdocbody').removeClass('markdown');
-      $('#underline').removeAttr('style'); // no markdown support for these
-      $('#strikethrough').removeAttr('style');
-    },
+  const enable = () => {
+    // add css class markdown
+    $('iframe[name="ace_outer"]').contents().find('iframe')
+        .contents().find('#innerdocbody').addClass('markdown');
+    $('#underline').hide(); // no markdown support for these
+    $('#strikethrough').hide();
+  };
+  const disable = () => {
+    // add css class markdown
+    $('iframe[name="ace_outer"]').contents().find('iframe')
+        .contents().find('#innerdocbody').removeClass('markdown');
+    $('#underline').removeAttr('style'); // no markdown support for these
+    $('#strikethrough').removeAttr('style');
   };
 
   /* init */
   if ($('#options-markdown').is(':checked')) {
-    markdown.enable();
+    enable();
   } else {
-    markdown.disable();
+    disable();
   }
   /* on click */
   $('#options-markdown').on('click', () => {
     if ($('#options-markdown').is(':checked')) {
-      markdown.enable();
+      enable();
     } else {
-      markdown.disable();
+      disable();
     }
   });
 };
